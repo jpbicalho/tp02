2fa2eab521f02dc8348b010d4b501b46dca5c5b8
bicaspvd <joaopedrosbicalho@gmail.com>
2024-01-24 16:02:07 -0300

    Att main

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 #include "arquivo.h"
 #include "heap.h"
 #include <time.h>
-int main()
+
+
+int main2f(int numReg,int ordemArquivo)
 {
     // variaveis para calcular o tempo
     clock_t inicio, fim;
@@ -12,33 +14,58 @@ int main()
     // variavei para calcular a contagem
     int numLeitura = 0, numEscrita = 0, numComparacao = 0;
     
-    // nome do arquivo que vai ser lido e quantidade de registros
-    int numReg = 20;
-    int i=0;
-    char nomeBase[]="PROVAO.TXT";
+    // nome do arquivo que vai ser lido 
+    char* nomeBase;
+    
+    
+    switch (ordemArquivo)
+    {
+    case 1:
+        nomeBase = "PROVAO_ASCENDENTE.TXT"
+        break;
+    case 2:
+        nomeBase = "PROVAO_DESCENDENTE.TXT"
+        break;
+    case 3:
+        nomeBase = "PROVAO_ALEATORIO.TXT"
+        break;
+    default:
+        printf("\nErro na escolha do arquivo");
+        return 0;
+        break;
+    }
 
     printf("\nCriacao das fitas para intercalacao...");
     criaFitas();
 
+
+
     printf("\n\n... Fase de selecao por substituicao ... ");
     selecao_por_substituicao(&numLeitura, &numEscrita, &numComparacao,nomeBase,numReg);
 
+
+
     printf("\n\n... Fase de intercalacao ... ");
+
+
     do
     {
         intercalacao_Balanceada_Fitas_entrada(&numLeitura, &numEscrita, &numComparacao);
         intercalacao_Balanceada_Fitas_Saida(&numLeitura, &numEscrita, &numComparacao);
-        i++;
+        
     } while (confere_Intercalacao() != numReg);
 
     fim = clock();
-    printf("\nnum de repeticoes: %d", i);
-    printf("\n\nTempo de execucao: %4f segundos\n\n", (double)(fim - inicio) / CLOCKS_PER_SEC);
-    printf("\nNumero de leitura: %d\n", numLeitura);
-    printf("\nNumero de escrita: %d\n", numEscrita);
-    printf("\nNumero de comparacoes: %d\n", numComparacao);
+    
+    
+    
+    printf("\n\nTempo de execucao:\t%4f segundos\n\n", (double)(fim - inicio) / CLOCKS_PER_SEC);
+    printf("\nNumero de leitura:\t%d\n", numLeitura);
+    printf("\nNumero de escrita:\t%d\n", numEscrita);
+    printf("\nNumero de comparacoes:\t%d\n", numComparacao);
     printf("\n\n\n...Gerando registros ordenados...");
     
+    //gera um arquivo com os registros ordenados em txt
     gera_registros_ordenados();
 
     return 0;
